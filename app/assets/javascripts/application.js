aec7bafa518240f1ba15737c7e73f64f1ba1e846
Sokly <hengsokly23@gmail.com>
2020-04-02 17:04:18 +0700

    add devise gem

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,19 @@
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+
+
+$(document).on("ready turbolinks:load", () => {
+
+  // const currentPage = OM.Util.getCurrentPage();
+  // if (OM[currentPage]) {
+  //   OM[currentPage].init();
+  // }
+
+  // OM.Common.MobileMenu.init();
+
+  // $("[data-toggle='tooltip']").tooltip()
+
+  // Rails.refreshCSRFTokens();
+});
+
