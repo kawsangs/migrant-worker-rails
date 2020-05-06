d7612dbef3e254ba0e62c1b7c608590663c2b610
Sokly <hengsokly23@gmail.com>
2020-05-06 14:29:23 +0700

    add about in signedin page

--- app/controllers/abouts_controller.rb
+++ app/controllers/abouts_controller.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 class AboutsController < ApplicationController
+  skip_before_action :authenticate_account!
+  layout :set_layout
+
   def show
   end
+
+  private
+    def set_layout
+      signed_in? ? 'layouts/application' : 'layouts/sidebar-less'
+    end
 end
