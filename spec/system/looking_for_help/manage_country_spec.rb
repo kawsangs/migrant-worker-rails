a0c4c1ab13194761e170e7de2929f0e9b7d469a7
radin reth <radin.reth@gmail.com>
2021-05-10 16:33:35 +0700

    Enhance country Ui

--- spec/system/looking_for_help/manage_country_spec.rb
+++ spec/system/looking_for_help/manage_country_spec.rb
@@ -26,7 +26,7 @@ RSpec.describe 'Manage country', type: :system do
     it 'enables to update existing country name' do
       visit '/countries'
 
-      click_link "edit"
+      click_link "edit_country_#{country.id}"
       fill_in 'Name', with: 'China'
       click_button 'save'
 
@@ -38,7 +38,7 @@ RSpec.describe 'Manage country', type: :system do
     it 'enables to delete existing country by name' do
       visit '/countries'
 
-      click_link "delete"
+      click_link "delete_country_#{country.id}"
 
       expect(page).to have_content('success')
     end
