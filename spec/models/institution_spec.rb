53ba361b144d47d73a309c25890c6a5b87780f32
radin reth <radin.reth@gmail.com>
2021-05-11 11:09:57 +0700

    Add institution model

---
+++ spec/models/institution_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Institution, type: :model do
+  it { is_expected.to have_db_column(:name) }
+  it { is_expected.to have_db_column(:kind) }
+  it { is_expected.to have_many(:contacts) }
+end
