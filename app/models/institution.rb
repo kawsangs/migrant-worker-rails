dd1db4dc2beb448f2969920a6a77a2f2f0a8e82a
radin reth <radin.reth@gmail.com>
2021-05-12 11:34:39 +0700

    Set dependent destroy for contact associations

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -1,7 +1,7 @@
 class Institution < ApplicationRecord
   enum kind: { ngo: 1, gov: 2, other: 3 }
   
-  has_many :contacts, inverse_of: :institution
+  has_many :contacts, inverse_of: :institution, dependent: :destroy
   has_many :country_institutions, dependent: :destroy
 
   accepts_nested_attributes_for :country_institutions, allow_destroy: true
