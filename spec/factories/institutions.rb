2edae1b1e17a337d4dbe967b4707a49a5b5bc4aa
radin reth <radin.reth@gmail.com>
2021-05-19 15:31:01 +0700

    Annotate factories

--- spec/factories/institutions.rb
+++ spec/factories/institutions.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: institutions
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  kind       :integer          default("gov")
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 FactoryBot.define do
   factory :institution do
     name { 'child help' }
