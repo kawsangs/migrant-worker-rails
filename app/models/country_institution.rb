a4ae74c68a0c64cef980cd87dfd3df5357019b4d
radin reth <radin.reth@gmail.com>
2021-05-19 18:10:34 +0700

    Delegate emoji flag to country model

--- app/models/country_institution.rb
+++ app/models/country_institution.rb
@@ -13,17 +13,13 @@ class CountryInstitution < ApplicationRecord
   belongs_to :institution
   belongs_to :country
 
-  delegate :emoji_flag, to: :country_iso
+  delegate :emoji_flag, to: :country
 
   after_initialize :set_country
-  
+
   private
 
   def set_country
     self.country = Country.find_or_create_by(name: self.country_code)
-  end
-
-  def country_iso
-    ISO3166::Country.find_country_by_name(country_code)
   end
 end
