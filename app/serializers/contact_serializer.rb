fd351248cd6f01e684015406049040ea1c410e12
radin reth <radin.reth@gmail.com>
2021-05-19 15:52:05 +0700

    Add more attributes & relations to countries and institution serializer

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: contacts
+#
+#  id             :bigint           not null, primary key
+#  type           :string           not null
+#  value          :string           default("")
+#  institution_id :bigint           not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :type, :value
 end
--- app/serializers/institution_serializer.rb
+++ app/serializers/institution_serializer.rb
@@ -1,5 +1,16 @@
+# == Schema Information
+#
+# Table name: institutions
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  kind       :integer          default("gov")
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class InstitutionSerializer < ActiveModel::Serializer
-  attributes :id, :name
+  attributes :id, :name, :kind
 
   has_many :contacts
+  has_many :country_institutions
 end
