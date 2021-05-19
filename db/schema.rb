0b01e62fbed477af35dd71e443fe9a7b89ea2cd6
radin reth <radin.reth@gmail.com>
2021-05-19 15:33:34 +0700

    Add country code

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_11_044734) do
+ActiveRecord::Schema.define(version: 2021_05_17_094850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,7 @@ ActiveRecord::Schema.define(version: 2021_05_11_044734) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "code", default: ""
     t.index ["name"], name: "index_countries_on_name", unique: true
   end
 
