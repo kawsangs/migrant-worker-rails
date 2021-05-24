4bd22a0200549aa1aea6f7b41de93e14bdeb5e7c
radin reth <radin.reth@gmail.com>
2021-05-24 18:07:32 +0700

    Update dev setup script to Install yarn packages

--- dev-setup.sh
+++ dev-setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 docker-compose build
+docker-compose run --rm web yarn install --check-files
 docker-compose run --rm web bundle install
 docker-compose run --rm web bundle exec rake db:setup
 docker-compose run --rm web bundle exec rake db:test:prepare
