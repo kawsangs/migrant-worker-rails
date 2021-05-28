7274e87726da7598290e681f666598ceff72c9f7
radin reth <radin.reth@gmail.com>
2021-05-28 15:59:10 +0700

    Fix institution serializer call active storage method

--- app/serializers/institution_serializer.rb
+++ app/serializers/institution_serializer.rb
@@ -11,18 +11,22 @@
 class InstitutionSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
-  attributes :id, :name, :kind, :address, :logo_url, :audio_url
+  attributes :id, :name, :kind, :address, :logo_url, :logo, :audio_url
 
   has_many :contacts
   has_many :country_institutions
 
+  def logo
+    "require('../../../app/assets/images/dummy/#{logo_url}')" if logo_url.present?
+  end
+
   def logo_url
-    return nil unless object.logo.attached?
-    object.logo.filename.to_s
+    return unless object.logo.file.present?
+    object.logo.url
   end
 
   def audio_url
-    return nil unless object.audio.attached?
-    object.audio.filename.to_s
+    return nil unless object.audio.file.present?
+    object.audio.identifier
   end
 end
