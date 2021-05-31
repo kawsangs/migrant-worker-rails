548432029a50b4f45d86f59755d9e87b5cdd815c
radin reth <radin.reth@gmail.com>
2021-05-31 12:08:24 +0700

    Split privacy policy from terms and conditions page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "migrants#index"
   get "/terms-and-conditions", to: 'terms_and_conditions#index'
+  get "/privacy-policy", to: 'privacy_policy#index'
 
   resources :accounts do
     post :update_locale, on: :collection
